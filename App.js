3340ca42a69d0bd8db5dc161c4d2483542e5aefa
Manuel <m_guzman74@yahoo.com>
2021-07-31 00:40:50 -0500

    added cache wrapper for future use & added offline notice

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer } from "@react-navigation/native";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 import AppNavigator from "./app/navigation/AppNavigator";
+import OfflineNotice from "./app/Components/OfflineNotice";
 import SearchScreen from "./app/screens/SearchScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import SearchAdvanced from "./app/screens/SearchAdvanced";
 
 export default function App() {
+  // const netInfo = useNetInfo();
   return (
-    <NavigationContainer>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
--- app/config/colors.js
+++ app/config/colors.js
@@ -1,6 +1,7 @@
 export default {
   accent: "#F07B49",
   cardBG: "#1D3437",
+  danger: "#ff5252",
   mainBG: "#022249",
   white: "white",
 };
