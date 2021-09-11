44f4a86c2aeb55d51dc33a23b2c9ed5fb79e6996
Manuel <m_guzman74@yahoo.com>
2021-09-10 19:49:12 -0600

    edits

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import jwtDecode from "jwt-decode";
 import AppLoading from "expo-app-loading";
