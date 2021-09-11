c790e5c7dbed422d8f2de257ea9ff5c54756e9c5
Manuel <m_guzman74@yahoo.com>
2021-09-10 19:44:03 -0600

    fixed splash screen flickering on reload

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import jwtDecode from "jwt-decode";
+import AppLoading from "expo-app-loading";
 
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthContext from "./app/auth/context";
@@ -10,15 +11,23 @@ import authStorage from "./app/auth/storage";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
     if (!token) return;
     setUser(jwtDecode(token));
   };
-  useEffect(() => {
-    restoreToken();
-  }, []);
+
+  if (!isReady)
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
