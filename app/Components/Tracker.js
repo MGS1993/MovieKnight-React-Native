ae604dded25c13252a0bf41e2f9a30da477f6dfd
Manuel <m_guzman74@yahoo.com>
2021-09-28 01:51:28 -0500

    basic notification worked out

--- app/Components/Tracker.js
+++ app/Components/Tracker.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 
 import AppButton from "./AppButton";
 import AuthContext from "../auth/context";
--- app/hooks/useNotifications.js
+++ app/hooks/useNotifications.js
@@ -1,5 +1,6 @@
 import { useEffect, useContext } from "react";
 import * as Notifications from "expo-notifications";
+import { Platform } from "react-native";
 
 import AuthContext from "../auth/context";
 import expoPushTokens from "../Util/expoPushTokens";
@@ -26,6 +27,32 @@ export default useNotifications = (notificationListener) => {
         userContext.user,
         "/registerToken"
       );
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: true,
+          shouldSetBadge: false,
+        }),
+      });
+
+      if (Platform.OS === "android") {
+        Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+
+      // Notifications.scheduleNotificationAsync({
+      //   content: {
+      //     title: "Time's up!",
+      //     body: "Change sides!",
+      //   },
+      //   trigger: {
+      //     seconds: 15,
+      //   },
+      // });
     } catch (error) {
       console.log("Error registering token", error);
     }
